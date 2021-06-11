3d6d68fc9265d241d8ec957ed4e1eb9fffd4b414
Loong Fu <longphu753951@gmail.com>
2021-06-12 00:39:00 +0700

    first commit

---
+++ AttendanceKnnPj.py
@@ -0,0 +1,41 @@
+from train import train
+from predict import predict
+import cv2
+import face_recognition
+import pickle
+from face_recognition.face_recognition_cli import image_files_in_folder
+
+
+
+if __name__ == "__main__":
+    print("Training KNN classifier...")
+    classifier = train("image_knn/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
+    print("Training complete!")
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = video_capture.read()
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Find all people in the image using a trained classifier model
+        # Note: You can pass in either a classifier file name or a classifier model instance
+        predictions = predict(X_img_path=rgb_frame, model_path="trained_knn_model.clf")
+
+        for name, (top, right, bottom, left) in predictions:
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Display the resulting image
+            cv2.imshow('Video', frame)
+
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
---
+++ AttendancePj.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'image'
+images = []
+classNames = []
+myList = os.listdir(path)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    print(os.path.split(cl))
+    classNames.append(os.path.split(cl)[1])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%m:%S')
+            f.writelines(f'\n{name},{dtString}')
+            
+
+encodeListKnown = findEncodings(images)
+print(classNames)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace,faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1,x2,y2,x1 = faceLoc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4 
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2+35),(x2,y2),(255,0,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2+6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+
+        cv2.imshow('Webcam',img)
+        cv2.waitKey(1)
